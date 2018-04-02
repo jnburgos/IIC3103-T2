cf016e556c2942585352617fdfffc19000d6db73
Javiera Burgos <jnburgos@uc.cl>
2018-04-02 19:55:00 -0300

    rollback migration

--- jb-onlineNews/db/migrate/20180402222321_remove_updated_at_from_articles.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUpdatedAtFromArticles < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :articles, :updated_at, :datetime
-  end
-end
--- jb-onlineNews/db/schema.rb
+++ jb-onlineNews/db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180402222321) do
     t.string   "subtitle"
     t.string   "body"
     t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comentarios", force: :cascade do |t|
