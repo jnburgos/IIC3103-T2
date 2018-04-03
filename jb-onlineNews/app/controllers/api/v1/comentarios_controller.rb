a600a5f8ad19754105faa9419bb52444462c3cd3
Javiera Burgos <jnburgos@uc.cl>
2018-04-03 14:47:03 -0300

    fix errors

--- jb-onlineNews/app/controllers/api/v1/comentarios_controller.rb
+++ jb-onlineNews/app/controllers/api/v1/comentarios_controller.rb
@@ -19,17 +19,17 @@ module Api
         @comentario = Comentario.find(params[:id])
 
 
-        render json: {status: 'SUCCESS', message:'Loaded article', data:@comentario},status: :ok
+        render json: @comentario,status: :ok
       end
 
       def create
-        comentario = Comentario.new(comentario_params)
+        @comentario = Comentario.new(comentario_params)
         @article = Article.find(params[:article_id])
-        comentario.article = @article
+        @comentario.article = article
         if comentario.save
-          render json: comentario, status: :created
+          render json: @comentario, status: :created
         else
-          render json: {status: 'ERROR', message:'Article not saved', data:comentario.errors},status: :unprocessable_entity
+          render json: @comentario.errors,status: :unprocessable_entity
         end
       end
 
@@ -37,16 +37,16 @@ module Api
         @comentarios = Comentario.find(params[:id])
 
         if @comentarios.update_attributes(comentario_params)
-          render json: {status: 'SUCCESS', message:'Updated comment', data:@comentarios},status: :ok
+          render json: @comentarios,status: :ok
         else
-          render json: {status: 'ERROR', message:'Comment not updated', data:@comentarios.errors},status: :unprocessable_entity
+          render json: @comentarios.errors,status: :unprocessable_entity
         end
       end
 
       def destroy
         @comentarios = Comentario.find(params[:id])
         @comentarios.destroy
-        render json: {status: 'SUCCESS', message:'Deleted comment', data: @comentarios},status: :ok
+        render json: comentarios,status: :ok
       end
 
       def comentario_params
